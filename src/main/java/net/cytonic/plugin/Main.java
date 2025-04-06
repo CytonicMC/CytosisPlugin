3b2383ee1ab78ca29922bbf4d23cb525076f98a0
foxikle <foxikle@cytonic.net>
2025-04-06 19:47:07 -0400

    fix usage

--- src/main/java/net/cytonic/plugin/Main.java
+++ src/main/java/net/cytonic/plugin/Main.java
@@ -1,9 +1,18 @@
 package net.cytonic.plugin;
 
 import net.cytonic.cytosis.plugins.CytosisPlugin;
-import net.cytonic.cytosis.plugins.Plugin;
+import net.cytonic.cytosis.plugins.annotations.Dependency;
+import net.cytonic.cytosis.plugins.annotations.Plugin;
 
-@Plugin(name = "example-plugin", version = "1.0-SNAPSHOT", description = "An example plugin for Cytosis", author = "Cytonic Developers", dependencies = {"Your dependency here"})
+@Plugin(
+        name = "example-plugin",
+        version = "1.0-SNAPSHOT",
+        description = "An example plugin for Cytosis",
+        authors = {"Cytosis Developers"},
+        dependencies = {
+                @Dependency(id = "example-dependency")
+        }
+)
 public class Main implements CytosisPlugin {
 
     @Override
