966746e2fd038d506d4172dac623c516e8f311a5
webhead1104 <webhead1104@hotmail.com>
2025-07-02 12:16:13 -0500

    Update stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,40 @@
 plugins {
     id("java")
+    id("com.gradleup.shadow") version "9.0.0-beta17"
+    id("io.freefair.lombok") version "8.14"
 }
 
-group = "net.cytonic"
+group = "net.cyonic"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    mavenLocal()
     maven("https://repo.foxikle.dev/cytonic")
+    maven("https://jitpack.io")
 }
 
 dependencies {
     compileOnly("net.cytonic:Cytosis:1.0-SNAPSHOT")
 }
 
-tasks.test {
-    useJUnitPlatform()
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+tasks {
+    assemble {
+        dependsOn(shadowJar)
+    }
+    jar {
+        archiveFileName.set("${project.name}.jar")
+    }
+    shadowJar {
+        archiveFileName.set("${project.name}.jar")
+        archiveClassifier.set("")
+        mergeServiceFiles()
+        if (providers.gradleProperty("server_dir").isPresent) {
+            destinationDirectory.set(file(providers.gradleProperty("server_dir").get() + "/plugins"))
+        }
+    }
 }
--- src/main/java/net/cytonic/plugin/Main.java
+++ src/main/java/net/cytonic/plugin/Main.java
@@ -1,18 +1,7 @@
 package net.cytonic.plugin;
 
 import net.cytonic.cytosis.plugins.CytosisPlugin;
-import net.cytonic.cytosis.plugins.annotations.Dependency;
-import net.cytonic.cytosis.plugins.annotations.Plugin;
 
-@Plugin(
-        name = "example-plugin",
-        version = "1.0-SNAPSHOT",
-        description = "An example plugin for Cytosis",
-        authors = {"Cytosis Developers"},
-        dependencies = {
-                @Dependency(id = "example-dependency")
-        }
-)
 public class Main implements CytosisPlugin {
 
     @Override
